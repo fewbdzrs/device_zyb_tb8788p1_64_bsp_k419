1f496c52632f64502de0ea25db3c37b0b7fc41da
fewbdzrs <3577181704@qq.com>
2026-09-13 03:55:38 +0800

    提交说明

--- omni_tb8788p1_64_bsp_k419.mk
+++ omni_tb8788p1_64_bsp_k419.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/fox/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from tb8788p1_64_bsp_k419 device
 $(call inherit-product, device/zyb/tb8788p1_64_bsp_k419/device.mk)
